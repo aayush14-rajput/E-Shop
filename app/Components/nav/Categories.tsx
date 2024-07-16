16204c14c2f79b9ee879facecb0df73fb1d8e9b8
ayush <ayush@gmail.com>
2024-07-16 16:33:06 +0530

    first commit

--- app/Components/nav/Categories.tsx
+++ app/Components/nav/Categories.tsx
@@ -1,44 +1,36 @@
 "use client"
-import React, { Suspense } from 'react';
-import Container from '../Container';
-import { categories } from '@/Utils/Categories';
-import Category from './Category';
-import { usePathname, useSearchParams } from 'next/navigation';
+import React from 'react'
+import Container from '../Container'
+import { categories } from '@/Utils/Categories'
+import Category from './Category'
+import { usePathname, useSearchParams } from 'next/navigation'
 
-const Categories = ({ category }) => {
-    const pathname = usePathname();
-    const isMainPage = pathname === '/';
+const Categories = () => {
+    const params=useSearchParams()
+    const category=params?.get('category')  //getting category from params
 
-    if (!isMainPage) return null;
+    const pathname=usePathname();
 
-    return (
-        <div className='bg-white'>
-            <Container>
-                <div className='pt-4 flex flex-row items-center justify-between overflow-x-auto'>
-                    {categories.map((items) => (
-                        <div key={items.label}>
-                            <Category 
-                                label={items.label} 
-                                icon={items.icon} 
-                                selected={category === items.label || (category === null && items.label === 'All')}
-                            />
-                        </div>
-                    ))}
+    const isMainPage=pathname==='/'
+
+    if(!isMainPage) return null;
+  return (
+    <div className='bg-white'>
+        <Container>
+            <div className='pt-4 flex flex-row items-center justify-between overflow-x-auto'>
+           {categories.map((items)=>{
+            return(
+                <>
+                <div>
+                  <Category key={items.label} label={items.label} icon={items.icon} selected={category===items.label ||  (category===null && items.label==='All')}/>
                 </div>
-            </Container>
-        </div>
-    );
-};
+                </>
+            )
+           })}
+            </div>
+        </Container>
+    </div>
+  )
+}
 
-const CategoriesWithParams = () => {
-    const params = useSearchParams();
-    const category = params?.get('category'); // getting category from params
-
-    return (
-        <Suspense fallback={<div>Loading...</div>}>
-            <Categories category={category} />
-        </Suspense>
-    );
-};
-
-export default CategoriesWithParams;
+export default Categories
